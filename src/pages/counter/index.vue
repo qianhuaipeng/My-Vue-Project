aece30809cbe8c95420e8fb047beb94908d7c527
alan.peng <alan.peng@xiaoi.com>
2018-06-29 18:14:16 +0800

    提交

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>Vuexs counters：{{ counts }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    },
+    counts () {
+      return store.state.count1
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
---
+++ src/pages/splash/index.vue
@@ -0,0 +1,62 @@
+
+<view class="container">
+  <swiper class="splash" indicator-dots>
+    <swiper-item wx:for="{{ movies }}" wx:for-index="index" wx:key="id">
+      <image src="{{ item.images.large }}" class="slide-image" mode="aspectFill"/>
+      <button class="start" bindtap="handleStart" v-if="{{ index == movies.length - 1 }}">立即体验</button>
+    </swiper-item>
+  </swiper>
+</view>
+
+<script>
+  export default {
+    data () {
+        return {
+          movies: [],
+          loading: true
+        }
+    },
+    methods : {
+
+    }
+  }
+</script>
+
+
+<style>
+  page {
+    height: 100%;
+  }
+  .container {
+    height: 100%;
+  }
+
+  .splash {
+    height: 100%;
+  }
+
+  .splash swiper-item {
+    flex: 1;
+  }
+
+  .splash swiper-item image {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    opacity: .9;
+  }
+
+  .start {
+    position: absolute;
+    bottom: 200rpx;
+    left: 50%;
+    width: 300rpx;
+    margin-left: -150rpx;
+    background-color: rgba(64, 88, 109, .4);
+    color: #fff;
+    border: 1rpx solid rgba(64, 88, 109, .8);
+    border-radius: 200rpx;
+    font-size: 40rpx;
+  }
+
+</style>
